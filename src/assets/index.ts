f71b9c882839deb6ccc306179237215972f74adb
leoAshu <ashutosh.ojha2009@gmail.com>
2024-01-09 20:17:46 -0800

    designs LinkCard component based on dummy data

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,6 @@
 import add from './add-icon.svg'
 import logo from './header-logo.svg'
 import roundButton from './round-button.svg'
+import menu from './menu.svg'
 
-export { add, logo, roundButton }
+export { add, logo, roundButton, menu }
--- src/popup/views/Home.tsx
+++ src/popup/views/Home.tsx
@@ -1,9 +1,48 @@
-import { roundButton } from '@assets/index.ts'
+import { roundButton, menu } from '@assets/index.ts'
+
+const links = [
+    {
+        id: 1,
+        title: 'LinkedIn',
+        link: 'www.linkedin.com/leoAshu',
+    },
+    {
+        id: 2,
+        title: 'GitHub',
+        link: 'www.linkedin.com/leoAshu',
+    },
+    {
+        id: 3,
+        title: 'Leetcode',
+        link: 'www.linkedin.com/leoAshu',
+    },
+    {
+        id: 4,
+        title: 'Portfolio',
+        link: 'www.linkedin.com/leoAshu',
+    },
+]
+
 const Home = () => {
     return (
-        <div className="flex">
-            <div className="absolute bottom-5 right-5 h-10 rounded-full shadow-md shadow-[#00000040] cursor-pointer active:scale-[0.9] transition-all ease-in-out duration-300">
+        <>
+            {links.map((link) => (
+                <LinkCard key={link.id} title={link.title} />
+            ))}
+
+            <div className="absolute bottom-4 right-4 h-10 rounded-full shadow-md shadow-[#00000040] cursor-pointer active:scale-[0.9] transition-all ease-in-out duration-300">
                 <img src={roundButton} />
+            </div>
+        </>
+    )
+}
+
+const LinkCard = ({ title }: { title: string }) => {
+    return (
+        <div className="bg-white mt-1 py-1 px-5 shadow flex justify-between items-center">
+            <p className="text-slate-500 text-xs">{title}</p>
+            <div className="w-8 h-8 p-2 hover:bg-gray-100 active:bg-gray-200 rounded-full cursor-pointer transition-all ease-in-out duration-300 flex justify-center items-center">
+                <img className="h-4 w-4" src={menu} />
             </div>
         </div>
     )
