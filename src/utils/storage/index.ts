dad4f1303c63d1bde082663d041e34492b5bf6dc
leoAshu <ashutosh.ojha2009@gmail.com>
2024-01-26 22:54:05 -0800

    removes log from fetchLinks

--- src/utils/storage/index.ts
+++ src/utils/storage/index.ts
@@ -2,8 +2,6 @@ import { Link } from '@src/models'
 
 const fetchLinks = async (): Promise<Link[]> => {
     const storedLinks = await chrome.storage.sync.get('links')
-    console.log(storedLinks, 'storedLinks')
-
     return storedLinks['links']
 }
 
