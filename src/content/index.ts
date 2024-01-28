6178e59ffdcf3b2a99a20190fa8e49e09bb50e76
leoAshu <ashutosh.ojha2009@gmail.com>
2024-01-28 13:46:47 -0800

    adds support for text area elements

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,8 +1,8 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === 'pasteLink') {
         const focusedInput = document.activeElement
-        if (focusedInput && focusedInput.tagName === 'INPUT') {
-            ;(focusedInput as HTMLInputElement).value += message.link
+        if (focusedInput && (focusedInput.tagName === 'INPUT' || focusedInput.tagName == 'TEXTAREA')) {
+            ;(focusedInput as HTMLInputElement | HTMLTextAreaElement).value += message.link
         }
     }
 })
