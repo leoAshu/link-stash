bd2d88ab2034e4bf176c3d3db7bd69b45a788cb0
leoAshu <ashutosh.ojha2009@gmail.com>
2024-01-28 13:39:16 -0800

    fixes error duplicate creation of context menu

--- src/background/index.ts
+++ src/background/index.ts
@@ -4,33 +4,39 @@ const createContextMenu = async () => {
     const storedLinks = await fetchLinks()
 
     if (storedLinks && storedLinks.length) {
-        chrome.contextMenus.create({
-            id: 'linkStash',
-            title: 'Links',
-            contexts: ['editable'],
-            documentUrlPatterns: ['http://*/*', 'https://*/*'],
-        })
+        chrome.contextMenus.create(
+            {
+                id: 'linkStash',
+                title: 'Links',
+                contexts: ['editable'],
+                documentUrlPatterns: ['http://*/*', 'https://*/*'],
+            },
+            () => chrome.runtime.lastError
+        )
 
         storedLinks.forEach((link) => {
-            chrome.contextMenus.create({
-                id: link.id,
-                title: link.title,
-                parentId: 'linkStash',
-                contexts: ['editable'],
-            })
+            chrome.contextMenus.create(
+                {
+                    id: link.id,
+                    title: link.title,
+                    parentId: 'linkStash',
+                    contexts: ['editable'],
+                },
+                () => chrome.runtime.lastError
+            )
         })
-
-        chrome.contextMenus.onClicked.addListener(
-            (info: chrome.contextMenus.OnClickData, tab?: chrome.tabs.Tab | undefined) => {
-                if (tab && tab.id) {
-                    chrome.tabs.sendMessage(tab.id, {
-                        action: 'pasteLink',
-                        link: cleanUrl(storedLinks.filter((link) => link.id === info.menuItemId.toString())[0].url),
-                    })
-                }
-            }
-        )
     }
+
+    chrome.contextMenus.onClicked.addListener(
+        (info: chrome.contextMenus.OnClickData, tab?: chrome.tabs.Tab | undefined) => {
+            if (tab && tab.id) {
+                chrome.tabs.sendMessage(tab.id, {
+                    action: 'pasteLink',
+                    link: cleanUrl(storedLinks.filter((link) => link.id === info.menuItemId.toString())[0].url),
+                })
+            }
+        }
+    )
 }
 
 chrome.runtime.onInstalled.addListener(createContextMenu)
--- src/content/index.ts
+++ src/content/index.ts
@@ -2,7 +2,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === 'pasteLink') {
         const focusedInput = document.activeElement
         if (focusedInput && focusedInput.tagName === 'INPUT') {
-            ;(focusedInput as HTMLInputElement).value = message.link
+            ;(focusedInput as HTMLInputElement).value += message.link
         }
     }
 })
